e7ecd1feb8e8bfc9974552c39139dbbe9a7a39f3
Derb777 <dima07032007@mail.ru>
2024-05-11 14:54:56 +0300

    boxes

--- core/src/com/mygdx/game/SpacewardBall.java
+++ core/src/com/mygdx/game/SpacewardBall.java
@@ -22,6 +22,42 @@ public class SpacewardBall extends Game {
 	Vector3 touch;
 	BitmapFont fontLarge, fontSmall;
 
+	Texture imgball1;
+	Texture imgball2;
+	Texture imgball3;
+	Texture imgball4;
+	Texture imgball5;
+
+	Texture imgBrick1;
+	Texture imgBrick2;
+	Texture imgBrick3;
+	Texture imgBrick4;
+	Texture imgBrick5;
+	Texture imgBrick6;
+	Texture imgBrick7;
+	Texture imgBrick8;
+	Texture imgBrick9;
+	Texture imgBrick10;
+	Texture imgBrick11;
+
+	Texture imgBricks1;
+	Texture imgBricks2;
+	Texture imgBricks3;
+	Texture imgBricks4;
+	Texture imgBricks5;
+	Texture imgBricks6;
+	Texture imgBricks7;
+	Texture imgBricks8;
+
+	Texture imgss1;
+	Texture imgss2;
+	Texture imgss3;
+
+	Texture imgsrt1;
+	Texture imgsrt2;
+	Texture imgsrt3;
+	Texture imgsrt4;
+
 	ScreenMenu screenMenu;
 	ScreenSettings screenSettings;
 	ScreenGame screenGame;
@@ -38,6 +74,45 @@ public class SpacewardBall extends Game {
 		touch = new Vector3();
 		fontLarge = new BitmapFont(Gdx.files.internal("crystalfont.fnt"));
 		fontSmall = new BitmapFont(Gdx.files.internal("crystalsmall.fnt"));
+
+		imgball1 = new Texture("ball1.png");
+		imgball2 = new Texture("ball2.png");
+		imgball3 = new Texture("ball3.png");
+		imgball4 = new Texture("ball4.png");
+		imgball5 = new Texture("ball5.png");
+
+		imgBrick1 = new Texture("Brick1.png");
+		imgBrick2 = new Texture("Brick2.png");
+		imgBrick3 = new Texture("Brick3.png");
+		imgBrick4 = new Texture("Brick4.png");
+		imgBrick5 = new Texture("Brick5.png");
+		imgBrick6 = new Texture("Brick6.png");
+		imgBrick7 = new Texture("Brick7.png");
+		imgBrick8 = new Texture("Brick8.png");
+		imgBrick9 = new Texture("Brick9.png");
+		imgBrick10 = new Texture("Brick10.png");
+		imgBrick11 = new Texture("Brick11.png");
+
+		imgBricks1 = new Texture("imgBricks1");
+		imgBricks2 = new Texture("imgBricks2");
+		imgBricks3 = new Texture("imgBricks3");
+		imgBricks4 = new Texture("imgBricks4");
+		imgBricks5 = new Texture("imgBricks5");
+		imgBricks6 = new Texture("imgBricks6");
+		imgBricks7 = new Texture("imgBricks7");
+		imgBricks8 = new Texture("imgBricks8");
+
+		imgss1 = new Texture("imgss1");
+		imgss2 = new Texture("imgss2");
+		imgss3 = new Texture("imgss3");
+
+		imgsrt1 = new Texture("imgsrt1");
+		imgsrt2 = new Texture("imgsr2");
+		imgsrt3 = new Texture("imgsr3");
+		imgsrt4 = new Texture("imgsrt4");
+
+
+
 
 		screenMenu = new ScreenMenu(this);
 		screenSettings = new ScreenSettings(this);
